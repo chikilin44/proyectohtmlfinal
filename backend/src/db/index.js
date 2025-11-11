514089b090731bccdb09188f19a562e70e0f5cc9
YarethLc <nykolle.londono01@uceva.edu.co>
2025-11-11 01:30:18 -0500

    conexion bd

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,14 +1,25 @@
+require('dotenv').config({ path:'C:\\Users\\gary\\Documents\\GitHub\\proyectohtmlfinal\\pedidos-yemm-project\\.env'});
 const { Pool } = require('pg');
-require('dotenv').config();
+console.log('📦 Variables cargadas:');
+console.log({
+  PGHOST: process.env.PGHOST,
+  PGPORT: process.env.PGPORT,
+  PGDATABASE: process.env.PGDATABASE,
+  PGUSER: process.env.PGUSER,
+  PGPASSWORD: process.env.PGPASSWORD
+});
 
 const pool = new Pool({
   host: process.env.PGHOST,
-  port: Number(process.env.PGPORT || 5432),
+  port: Number(process.env.PGPORT || 5433),
   database: process.env.PGDATABASE,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   max: 10,
   idleTimeoutMillis: 30000
 });
+pool.connect()
+  .then(() => console.log('✅ Conectado a PostgreSQL'))
+  .catch(err => console.error('❌ Error de conexión a PostgreSQL:', err));
 
 module.exports = pool;
